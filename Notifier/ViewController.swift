f8142f27a629b46ae6f1c5ccae83e00d7e75b6ec
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-10-16 12:30:26 +0300

    Initial Commit

---
+++ Notifier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notifier
+//
+//  Created by Abdulrahman on 10/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
