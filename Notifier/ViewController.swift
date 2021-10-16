18a6fb0c9894ce271ce3fc9f8657686e7dc6dd0a
Abdulrahman Qabbout <abdulrahman.qabbout@gmail.com>
2021-10-16 12:55:36 +0300

    added register and schedule buttons

--- Notifier/ViewController.swift
+++ Notifier/ViewController.swift
@@ -4,14 +4,55 @@
 //
 //  Created by Abdulrahman on 10/16/21.
 //
-
+import UserNotifications
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Register", style: .plain, target: self, action: #selector(registerLocal))
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Schedule", style: .plain, target: self, action: #selector(scheduleLocal))
+    }
+    
+    @objc func registerLocal(){
+        
+        let center = UNUserNotificationCenter.current()
+        center.requestAuthorization(options: [.alert,.badge,.sound]){
+            granted, error in
+            
+            if granted {
+                print("yay")
+                
+            } else {
+                print("oh no")
+            }
+        }
+    }
+    
+    @objc func scheduleLocal(){
+        let center = UNUserNotificationCenter.current()
+        center.removeAllPendingNotificationRequests()
+        
+        let content = UNMutableNotificationContent()
+        content.title = "Late wake up call"
+        content.body = "The early bird catches the worm, but the second mouse gets the cheese"
+        content.categoryIdentifier = "alarm"
+        content.userInfo = ["CustomData": "FizzBuzz"]
+        
+        content.sound = .default
+        
+        var dateComponent = DateComponents()
+        dateComponent.hour = 10
+        dateComponent.minute = 40
+//        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponent, repeats: true)
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        center.add(request)
     }
 
 
